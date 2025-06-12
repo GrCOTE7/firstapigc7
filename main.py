63c85863bb1b42bd2722bccfeab1f0209d170400
GrCOTE7 <grcote7@gmail.com>
2025-06-12 17:31:59 +0200

    Up

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI, from GrCOTE7!"}
